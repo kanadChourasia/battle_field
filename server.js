ddc467674096167d70dc186f6672ce3451e2e5c4
Kanad <kanadchourasia@gmail.com>
2018-11-01 19:00:32 +0530

    started queries

--- server.js
+++ server.js
@@ -1,14 +1,26 @@
-var express = require("express")
+var express = require('express');
+var jsend = require('jsend');
+require('dotenv').config();
+require('./lib/dbi/db');
+
 var app = express();
 
-app.get("*",function(req,res){
+app.use(jsend.middleware);
+
+app.get("*",(req,res)=>{
 	
 	res.send("<H1>App is Running</H1>")
 })
 
 
-app.listen(3000,function(err){
+app.listen(3000,(err)=>{
    if(err) throw err
    console.log("Yippee App is running on port 3000")
 })
+
+module.exports = {
+    app
+}
+
+require('./routes/routes');
 

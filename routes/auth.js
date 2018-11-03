25b59bdbf69b6d3c7134aa726c93a6097a585c09
kanad.puh <kanad.puh@gmail.com>
2018-11-04 05:12:40 +0530

    added search and auth

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,15 @@
 var app = require('./../server').app;
+var Auth = require('./../lib/task_functions/auth');
 
 app.post('/login',(req,res)=>{
-
+    let authObj = new Auth();
+    let body = req.body;
+    authObj.authenticate(body,(error,jwtSign)=>{
+        if(error){
+            res.jsend.error(error)
+        }
+        else{
+            res.jsend.success(jwtSign)
+        }
+    })
 })
--- routes/battels.js
+++ routes/battels.js
@@ -1,7 +1,8 @@
 var app = require('./../server').app;
 var Battles = require('./../lib/task_functions/battles');
+var appMiddleware = require('./../lib/task_functions/appMiddlewares')
 
-app.get('/list',(req,res)=>{
+app.get('/list',appMiddleware.verifyAuthenticity,(req,res)=>{
     let battlesObj = new Battles();
     battlesObj.getAllBattlePlaces((error,battlePlaces)=>{
         if(error){
@@ -13,7 +14,7 @@ app.get('/list',(req,res)=>{
     })
 })
 
-app.get('/count',(req,res)=>{
+app.get('/count',appMiddleware.verifyAuthenticity,(req,res)=>{
     let battleObj = new Battles();
     battleObj.getAllBattleCount((error,battleCount)=>{
         if(error){
@@ -25,7 +26,7 @@ app.get('/count',(req,res)=>{
     })
 })
 
-app.get('/stats',(req,res)=>{
+app.get('/stats',appMiddleware.verifyAuthenticity,(req,res)=>{
     let battleObj = new Battles();
     battleObj.getBattleStats((error,stats)=>{
         if(error){
@@ -33,6 +34,18 @@ app.get('/stats',(req,res)=>{
         }
         else{
             res.jsend.success(stats)
+        }
+    })
+})
+
+app.get('/search',appMiddleware.verifyAuthenticity,(req,res)=>{
+    let battleObj = new Battles();
+    battleObj.searchOptions(req.query,(error,result)=>{
+        if(error){
+            res.jsend.error(error.message)
+        }
+        else{
+            res.jsend.success(result)
         }
     })
 })
